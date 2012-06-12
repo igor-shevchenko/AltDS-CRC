0f5174528f644232d9241c8dfbaabcca3ea31d03
Igor Shevchenko <phar0s@ya.ru>
2012-06-12 20:42:08 +0500

    write CRC

--- ascrc.c
+++ ascrc.c
@@ -5,6 +5,7 @@
 #include "crc.h"
 
 const int BUF_SIZE = 1024;
+const char APPENDIX[5] = ":crc\0";
 
 void HandleFolder(char* folder, char recursive){
     WIN32_FIND_DATA FindFileData;
@@ -41,11 +42,11 @@ void HandleFolder(char* folder, char recursive){
 unsigned long FileCRC(char* filename){
     FILE *F;
     F = fopen(filename, "rb");
-    if(F != NULL) {
+    if (F != NULL){
 		unsigned char buf[BUF_SIZE];
 		unsigned long crc = 0;
 		int cnt;
-		do {
+		do{
 			cnt = fread(buf, 1, BUF_SIZE, F);
 			crc = crc_cycle(crc, buf, cnt);
 		} while(cnt == BUF_SIZE);
@@ -61,10 +62,33 @@ char CompareCRC(char* filename, unsigned long crc){
     return FileCRC(filename) == crc;
 }
 
+void WriteCRC(char *filename){
+    char streamFilename[MAX_PATH];
+    FILE *F;
+    strcpy(streamFilename, filename);
+    strcat(streamFilename, APPENDIX);
+    printf("%s", streamFilename);
+    F = fopen(streamFilename, "wb");
+    if (F != NULL){
+        unsigned long crc = FileCRC(filename);
+        int cnt;
+        cnt = fwrite(&crc, sizeof(crc), 1, F);
+        fclose(F);
+        if (!cnt){
+            printf("Unable to write to %s\n", streamFilename);
+            exit(-1);
+        }
+        
+    } else {
+        printf("Unable to open %s\n", streamFilename);
+        exit(-1);
+    }
+}
 
 int main(int argc, char* argv[]){
     setlocale(LC_ALL, "Russian_Russia");
     crc_init();
-
+    WriteCRC(argv[1]);
+    //HandleFolder(argv[1], 1);
     return (0);
 }
